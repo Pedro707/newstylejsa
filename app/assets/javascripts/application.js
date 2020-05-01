43b3cd24296f3a14e520a20ca54cc19bb3b971da
Mark Bateman <markbateman.digitaldwp@gmail.com>
2020-05-01 16:59:59 +0100

    Session timeout design implementation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,3 +8,70 @@ if (window.console && window.console.info) {
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
 })
+
+// Accessible Modal
+$(document).ready(function() {
+  var findModal = function(elem) {
+
+    var tabbable = elem.find('select, input, textarea, button, a').filter(':visible');
+
+    var firstTabbable = tabbable.first();
+    var lastTabbable = tabbable.last();
+    // set focus on first tabbable element
+    firstTabbable.focus();
+
+    // send last tabbable back to first
+    lastTabbable.on('keydown', function (e) {
+       if ((e.which === 9 && !e.shiftKey)) {
+           e.preventDefault();
+           firstTabbable.focus();
+       }
+    });
+
+    // send last shift tabbable back to last
+    firstTabbable.on('keydown', function (e) {
+        if ((e.which === 9 && e.shiftKey)) {
+            e.preventDefault();
+            lastTabbable.focus();
+        }
+    });
+
+    // allow esc to close Modal
+    elem.on('keyup', function(e){
+      if (e.keyCode === 27 ) {
+        elem.hide();
+        $('.govuk-modal-overlay').hide();
+        $('html').removeClass("noscroll");
+      };
+    });
+  };
+
+  // show modal and overlay and lock scroll
+  // $('.show-modal').click(function(e){
+  //   e.preventDefault();
+  //
+  //   $('.govuk-modal').show();
+  //   $('.govuk-modal-overlay').show();
+  //   $('html').addClass("noscroll");
+  //   findModal($('.govuk-modal'));
+  // });
+
+  if(document.querySelector(".govuk-modal")){
+    setTimeout(function(){
+      $('.govuk-modal').show();
+      $('.govuk-modal-overlay').show();
+      $('html').addClass("noscroll");
+      findModal($('.govuk-modal'));
+    }, 5000); // timed delay in nano-seconds (set to 5 seconds)
+  }
+
+  //hide modal and overlay
+  $('.close-modal').click(function(e){
+    e.preventDefault();
+
+    $('.govuk-modal').hide();
+    $('.govuk-modal-overlay').hide();
+    $('html').removeClass("noscroll");
+  });
+
+});
