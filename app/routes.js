b98011ffc62bb906e74a7cbea0f2ab53eef6f363
Mark Bateman <markbateman.digitaldwp@gmail.com>
2020-06-06 23:08:46 +0100

    Citizen version 1.3 implemented

--- app/routes.js
+++ app/routes.js
@@ -12,6 +12,57 @@ router.get('/', function (req, res) {
 
 ///////////////////////////////////////////////////////////////////////////////
 //                                                                           //
+//  ROUTING FOR VERSION 1.3                                                  //
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
+// This moves eligibility routing to eligibility directory
+router.use('/v1_3-citizen/1-eligibility/', require('./views/v1_3-citizen/1-eligibility/_routes'))
+
+// This moves `abroad` routing to `abroad` directory
+router.use('/v1_3-citizen/2-claim-start/', require('./views/v1_3-citizen/2-claim-start/_routes'))
+
+// This moves details routing to details directory
+router.use('/v1_3-citizen/3-details/', require('./views/v1_3-citizen/3-details/_routes'))
+
+// Redirect to first question in details sequence
+router.get('/v1_3-citizen/3-details/', function (req, res) {
+    res.redirect('/v1_3-citizen/3-details/nino')
+})
+
+// This moves `other-benefits` routing to `other-benefits` directory
+router.use('/v1_3-citizen/4-other-benefits/', require('./views/v1_3-citizen/4-other-benefits/_routes'))
+
+// This moves jury-service routing to jury-service directory
+router.use('/v1_3-citizen/5-jury-service/', require('./views/v1_3-citizen/5-jury-service/_routes'))
+
+// This moves current-employment routing to current-employment directory
+router.use('/v1_3-citizen/6-current-employment/', require('./views/v1_3-citizen/6-current-employment/_routes'))
+
+// This moves previous-employment routing to previous-employment directory
+router.use('/v1_3-citizen/7-previous-employment/', require('./views/v1_3-citizen/7-previous-employment/_routes'))
+
+// This moves `abroad` routing to `abroad` directory
+router.use('/v1_3-citizen/8-abroad/', require('./views/v1_3-citizen/8-abroad/_routes'))
+
+// This moves `pensions` routing to `pensions` directory
+router.use('/v1_3-citizen/9-pensions/', require('./views/v1_3-citizen/9-pensions/_routes'))
+
+// This moves `education` routing to `education` directory
+router.use('/v1_3-citizen/10-education/', require('./views/v1_3-citizen/10-education/_routes'))
+
+// This moves `availability` routing to `availability` directory
+router.use('/v1_3-citizen/11-availability/', require('./views/v1_3-citizen/11-availability/_routes'))
+
+// This moves `error` routing to `error` directory
+router.use('/v1_3-citizen/error/', require('./views/v1_3-citizen/error/_routes'))
+
+// {END OF ROUTING FOR VERSION 1.3} ///////////////////////////////////////////
+
+/*    */
+
+///////////////////////////////////////////////////////////////////////////////
+//                                                                           //
 //  ROUTING FOR VERSION 1.2                                                  //
 //                                                                           //
 ///////////////////////////////////////////////////////////////////////////////
